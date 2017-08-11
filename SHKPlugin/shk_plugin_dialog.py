a1b2e5b3535f9da74d5bb2ef82820627f127fd4c
ChrFr <christoph.franke@web.de>
2017-08-11 14:10:33 +0200

    Update: SHK-Plugin - load filters from xml, values may have children

--- SHKPlugin/shk_plugin_dialog.py
+++ SHKPlugin/shk_plugin_dialog.py
@@ -23,16 +23,18 @@
 """
 
 import os
-
+try:
+    from qgis.core import QgsDataSourceURI, QgsVectorLayer, QgsMapLayerRegistry
+except: pass
 from PyQt4 import QtGui, uic, QtCore
+import numpy as np
+from xml.etree import ElementTree as ET
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'shk_plugin_dialog_base.ui'))
 
 from config import Config
 from connection import DBConnection, Login
-from qgis.core import QgsDataSourceURI, QgsVectorLayer, QgsMapLayerRegistry
-import numpy as np
 
 config = Config()
 
@@ -40,10 +42,28 @@ SCHEMA = 'einrichtungen'
 
 
 class Filter(object):
-    def __init__(self, displayname, options):
-        self.displayname = displayname
-        self.options = options
-        self.active = self.options[0]
+    def __init__(self, column, values):
+        self.column = column
+        self.values = values
+        self.active_filters = []
+    
+    #@property
+    #def where(self):
+        #return "WHERE {} IN ({})".format(
+            #",".join(''.format(a) for a in self.active_filters))
+
+
+class LabeledSlider(QtGui.QWidget):
+    def __init__(self, min, max):
+        super(LabeledSlider, self).__init__()
+        self.min_label = QtGui.QLabel(str(min))
+        self.max_label = QtGui.QLabel(str(max))
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        layout = QtGui.QHBoxLayout()
+        layout.addWidget(self.min_label)
+        layout.addWidget(self.slider)
+        layout.addWidget(self.max_label)
+        self.setLayout(layout)
 
 
 class SHKPluginDialog(QtGui.QMainWindow, FORM_CLASS):
@@ -59,10 +79,17 @@ class SHKPluginDialog(QtGui.QMainWindow, FORM_CLASS):
         self.load_config()
         self.save_button.clicked.connect(self.save_config)
         self.connect_button.clicked.connect(self.connect)
-        self.layers = {'Bildungseinrichtungen': ('bildung_gesamt', self.schools_tree),
-                       'Medizinische Versorgung': ('gesundheit_gesamt', self.medicine_tree),
-                       'Nahversorgung': ('nahversorgung_gesamt', self.supply_tree)}
+        
+        self.layers = {
+            'Bildungseinrichtungen': ('bildung_gesamt', self.schools_tree),
+            'Medizinische Versorgung': ('gesundheit_gesamt', self.medicine_tree),
+            'Nahversorgung':  ('nahversorgung_gesamt', self.supply_tree)
+        }
+        
         self.filters = dict([(v, []) for v in self.layers.keys()])
+        
+        for (table, tree) in self.layers.itervalues():
+            tree.itemClicked.connect(check_tree)
     
     def load_config(self):
         db_config = config.db_config
@@ -115,58 +142,112 @@ class SHKPluginDialog(QtGui.QMainWindow, FORM_CLASS):
         self.init_filters()
             
     def init_filters(self):
-        table_sql = """
-        SELECT spalte
-        FROM meta.filter_einrichtungen
-        WHERE tabelle = '{table}'
-        """
+        fn = os.path.join(os.path.split(__file__)[0], "filter.xml")
+        root = ET.parse(fn).getroot()
+        table_filters = dict([(c.attrib['name'], c.getchildren())
+                              for c in root.getchildren()])
+        for layername, (tablename, tree) in self.layers.iteritems():
+            tree.clear()
+            filter_nodes = table_filters[tablename]
+            item = QtGui.QTreeWidgetItem(tree, ['Spalten'])
+            item.setExpanded(True)
+            for child in filter_nodes:
+                self.add_filter_node(item, child, tablename, tree)
+            tree.resizeColumnToContents(0)
+            #self.filters[layername] = filters
+            
+    def add_filter_node(self, parent_item, node, tablename, tree, where=''):
         column_sql = """
         SELECT "{column}"
         FROM {schema}.{table}
         """
-        for layername, (tablename, tree) in self.layers.iteritems():
-            tree.clear()
-            res = self.db_conn.fetch(table_sql.format(table=tablename))
-            filters = []
-            for r in res:
-                column = r[0]
-                values = self.db_conn.fetch(column_sql.format(
-                    column=column, table=tablename, schema=SCHEMA))
-                values = [v[0].strip() if v[0] else '' for v in values]
-                options = np.unique(values)
-                filters.append(Filter(column, options))
+        alias = node.attrib['alias'] if node.attrib.has_key('alias') else None
+        display_name = alias or node.attrib['name']
+        item = None
+        if node.tag == 'column':
+            item = QtGui.QTreeWidgetItem(parent_item, [display_name])
+            item.setCheckState(0, QtCore.Qt.Unchecked)
+            item.setFlags(QtCore.Qt.ItemIsUserCheckable |
+                          QtCore.Qt.ItemIsEnabled)
+            column = node.attrib['name'].encode('utf-8')
+            if where:
+                column_sql += ' WHERE ' + where
+            values = self.db_conn.fetch(column_sql.format(
+                column=column, table=tablename, schema=SCHEMA))
+            #stripped = []
+            #for value, in values:
+                #if type(value) == str:
+                    #value = value.strip()
+                #elif value is None:
+                    #continue
+                #stripped.append(value)
+
+            if node.attrib.has_key('input') and node.attrib['input'] == 'range':
+                values = [v for v, in values if v is not None]
+                v_min = np.min(values)
+                v_max = np.max(values)
+                slider = LabeledSlider(v_min, v_max)
+                tree.setItemWidget(item, 1, slider)
+            else:
+                values = ['' if v is None else v for v, in values]
+                options = np.sort(np.unique(values))
+                for o in options:
+                    option = QtGui.QTreeWidgetItem(item, [o])
+                    option.setCheckState(0, QtCore.Qt.Unchecked)
+                    option.setFlags(QtCore.Qt.ItemIsUserCheckable |
+                                    QtCore.Qt.ItemIsEnabled)
                 
-                col_item = QtGui.QTreeWidgetItem(tree, [column])
-                col_item.setCheckState(0, QtCore.Qt.Unchecked)
-                col_item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-                for option in options:
-                    opt_item = QtGui.QTreeWidgetItem(col_item, [option])
-                    opt_item.setCheckState(0, QtCore.Qt.Unchecked)
-                    opt_item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-            tree.resizeColumnToContents(0)
-            self.filters[layername] = filters
+                item.filter = Filter(column, options)
+            where = ''
             
-    def render_structure(self):
-        idx = self.year_combo.currentIndex()
-        # nothing selected (e.g. when triggered on clearance)
-        if idx < 0:
-            return
-        self.structure_tree.clear()
-        year = str(self.year_combo.currentText())
-        structure = self.db_conn.get_structure_available(year)
-        for cat, cols in structure.iteritems():
-            cat_item = QtGui.QTreeWidgetItem(self.structure_tree, [cat])
-            cat_item.setCheckState(0,QtCore.Qt.Unchecked)
-            cat_item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-            for col in cols:
-                col_item = QtGui.QTreeWidgetItem(cat_item, [col['name']])
-                col_item.setText(1, _fromUtf8(col['description']))
-                col_item.setCheckState(0,QtCore.Qt.Unchecked)
-                col_item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-        self.structure_tree.resizeColumnToContents(0)    
+        elif node.tag == 'value':
+            value = node.attrib['name']
+            # search parent for entry; if exists, add children to found one
+            found = None
+            for i in range(parent_item.childCount()):
+                child = parent_item.child(i)
+                print child.text(0)
+                if child.text(0) == value:
+                    found = child
+                    break
+            item = found
+            if hasattr(parent_item, 'filter'):
+                parent_filter = parent_item.filter
+                where = """"{c}" = '{v}'""".format(c=parent_filter.column, v=value)
+            
+        if item:
+            for child in node.getchildren():
+                self.add_filter_node(item, child, tablename, tree, where)
+                    
+def check_tree(item):
+    # check or uncheck all direct children
+    if item.checkState(0) != QtCore.Qt.PartiallyChecked:
+        state = item.checkState(0)
+        child_count = item.childCount()
+        for i in range(child_count):
+            item.child(i).setCheckState(0, state)
+
+    parent = item.parent()
+    while (parent and parent.text(0) != 'Spalten'):
+        # check/uncheck/partial check of parent of given item, depending on number of checked children
+        child_count = parent.childCount()
+        checked_count = 0
+        for i in range(child_count):
+            if (parent.child(i).checkState(0) == QtCore.Qt.Checked or
+                parent.child(i).checkState(0) == QtCore.Qt.PartiallyChecked):
+                checked_count += 1
+        if checked_count == 0:
+            parent.setCheckState(0, QtCore.Qt.Unchecked)
+        elif checked_count == child_count:
+            parent.setCheckState(0, QtCore.Qt.Checked)
+        else:
+            parent.setCheckState(0, QtCore.Qt.PartiallyChecked)
+            
+        parent = parent.parent()
         
 if __name__ == '__main__':
-    print ('hallo')
+    print
+    
         
         
     
